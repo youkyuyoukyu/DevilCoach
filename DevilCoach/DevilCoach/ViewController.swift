ba9ac8b62341db7e4b6f8534fac9193edfe0be9b
kira1021 <dldbdjq@gmail.com>
2019-06-26 15:03:56 +0900

    Start

---
+++ DevilCoach/DevilCoach/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevilCoach
+//
+//  Created by Kira on 26/06/2019.
+//  Copyright © 2019 Kira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
